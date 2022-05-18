f1da6b2e329f7195ee9264c19f1587577745bdd1
Christopher Larsen <christopher@deadratgames.com>
2022-05-18 12:58:57 -0400

    New Project

---
+++ BitcoinMillionaire/BitcoinMillionaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitcoinMillionaire
+//
+//  Created by Christopher Larsen on 2022-05-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
