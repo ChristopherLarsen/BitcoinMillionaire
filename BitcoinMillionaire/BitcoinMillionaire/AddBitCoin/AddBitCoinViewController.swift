28e41b130146825af96dbbb383710de03dbb05f8
Jorge Mattei <jxmattei@gmail.com>
2022-05-24 11:28:09 -0400

    wip

--- BitcoinMillionaire/BitcoinMillionaire/AddBitCoin/AddBitCoinViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AddBitcoinViewController.swift
-//  BitcoinMillionaire
-//
-//  Created by Karan Bhasin on 18/05/22.
-//
-
-import Foundation
---
+++ BitcoinMillionaire/BitcoinMillionaire/Home/BorderedTextField.swift
@@ -0,0 +1,27 @@
+//
+//  BorderedTextField.swift
+//  BitcoinMillionaire
+//
+//  Created by Jorge Mattei on 5/23/22.
+//
+
+import Foundation
+import UIKit
+ 
+class BorderedTextField : UITextField {
+    
+    let padding = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+    
+}
